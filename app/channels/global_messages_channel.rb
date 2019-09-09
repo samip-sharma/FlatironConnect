0777a2128e9a8e988c70fcaf8d7690f71cdc71a3
github username <github email address>
2019-09-09 15:21:49 -0400

    fixed bugs

--- app/channels/global_messages_channel.rb
+++ app/channels/global_messages_channel.rb
@@ -1,9 +1,11 @@
 class GlobalMessagesChannel < ApplicationCable::Channel
   def subscribed
+    puts "I am subscribed to this channel"
     stream_from "global_messages_channel"
   end
 
   def unsubscribed
+    puts "I am unsubscribed from this channel"
     # Any cleanup needed when channel is unsubscribed
   end
 end
