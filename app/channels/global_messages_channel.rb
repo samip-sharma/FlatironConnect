fa8d7df401edbaed00e8598bfd6bde2e38513d55
github username <github email address>
2019-09-11 11:27:40 -0400

    fixing action cable

--- app/channels/global_messages_channel.rb
+++ app/channels/global_messages_channel.rb
@@ -1,10 +1,18 @@
 class GlobalMessagesChannel < ApplicationCable::Channel
   def subscribed
     puts "I am subscribed to this channel"
+    # byebug
+    
+    user=User.find(params[:user])
+    user.active_user=true
+    user.save
     stream_from "global_messages_channel"
   end
 
   def unsubscribed
+    user=User.find(params[:user])
+    user.active_user=false
+    user.save
     puts "I am unsubscribed from this channel"
     # Any cleanup needed when channel is unsubscribed
   end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id ,:name , :user_name ,:admin, :working , :working_at
+  attributes :id ,:name , :user_name ,:admin, :working , :working_at, :active_user
   has_many :tweets
   has_many :blogs
   has_one :user_mod
